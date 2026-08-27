51ca8299ffd1943247fdd4352c85e55573b27aa6
RISC-1145 <wtpp647@gmail.com>
2026-08-27 20:50:12 +0800

    Add RED DSP LLVM backend and C-to-assembly compilation pipeline

--- bin/examples/test.c
+++ bin/examples/test.c
@@ -1,22 +1,32 @@
+int test(int a, int b, int c);
 
+int main() { return test(12, 18, 24); }
 
 int test(int a, int b, int c) {
-  if (a >= b) {
-    if (b >= c)
-      return b;
-    if (a >= c)
-      return c;
-    return a;
-  } else {
-    if (a >= c)
-      return a;
-    if (b >= c)
-      return c;
-    return b;
+  if (a == 0 || b == 0 || c == 0) {
+    return 0;
   }
-}
 
-int main() {
-  int result = test(2, 6, 7);
-  return result;
+  int x = a;
+  int y = b;
+  int r;
+  while (y != 0) {
+    r = x % y;
+    x = y;
+    y = r;
+  }
+  int gcd_ab = x;
+  int lcm_ab = a / gcd_ab * b;
+
+  x = lcm_ab;
+  y = c;
+  while (y != 0) {
+    r = x % y;
+    x = y;
+    y = r;
+  }
+  int gcd_abc = x;
+  int lcm_abc = lcm_ab / gcd_abc * c;
+
+  return lcm_abc;
 }
